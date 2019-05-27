6480aebcda2197b0992dc1a26aba26068a0f8efe
zy <1037236503@qq.com>
2019-05-28 00:16:59 +0800

    learn reactor pattern by muduo:s01

---
+++ reactor/s01/Eventloop.cpp
@@ -0,0 +1,81 @@
+//
+// Created by zy on 5/26/19.
+//
+
+#include "Eventloop.h"
+
+#include <poll.h>
+
+#include "base/Logging.h"
+#include "Poller.h"
+#include "Channel.h"
+
+using namespace muduo;
+
+
+thread_local Eventloop* t_loopInThisThread = nullptr;
+namespace
+{
+const int kPollTimeMs = 500;
+}
+
+Eventloop::Eventloop()
+    : looping_(false), threadId_(CurrentThread::tid()),
+      quit_(false)
+{
+    poller_.reset(new Poller(this));
+    LOG_TRACE << "Eventloop created " << this << " in thread " << threadId_;
+
+    if (t_loopInThisThread)
+    {
+        LOG_FATAL << "Another Eventloop " << t_loopInThisThread << " exists in this thread " << threadId_;
+    }
+    else
+    {
+        t_loopInThisThread = this;
+    }
+}
+
+Eventloop::~Eventloop()
+{
+    assert(!looping_);
+    t_loopInThisThread = nullptr;
+}
+
+void Eventloop::abortNotInLoopThread()
+{
+    LOG_ERROR << "Not in loop thread";
+    abort();
+}
+
+Eventloop* Eventloop::getEventloopOfCurrentThread()
+{
+    return t_loopInThisThread;
+}
+
+void Eventloop::loop()
+{
+    assert(!looping_);
+    assertInLoopThread();
+
+    looping_ = true;
+    quit_ = false;
+
+    while (!quit_) {
+        poller_->poll(kPollTimeMs, &activeChannels_);
+        for (auto& channel : activeChannels_) {
+            channel->handleEvent();
+        }
+    }
+
+    LOG_TRACE << "Eventloop " << this << " stop looping";
+    looping_ = false;
+}
+
+void Eventloop::updateChannel(muduo::Channel *channel)
+{
+    assert(channel->ownloop() == this);
+    assertInLoopThread();
+
+    poller_->updateChannel(channel);
+}
---
+++ reactor/s01/Poller.h
@@ -0,0 +1,60 @@
+//
+// Created by zy on 5/27/19.
+//
+
+#ifndef S01_POLLER_H
+#define S01_POLLER_H
+
+#include <vector>
+#include <map>
+
+#include <poll.h>
+
+#include "base/noncopyable.h"
+#include "Eventloop.h"
+#include "base/Timestamp.h"
+
+namespace muduo {
+class Channel;
+
+/*
+ *  IO Multiplexing with poll
+ *  This class doesn't own the Channel objects
+ * */
+class Poller : public noncopyable
+{
+public:
+    using ChannelList = std::vector<Channel*>;
+
+    Poller(Eventloop* loop);
+    ~Poller();
+
+    /*
+     * Polls the IO events
+     * Must be called in the loop thread
+     * */
+    Timestamp poll(int timeoutMs, ChannelList* activeChannels);
+
+    /*
+     * Changes the interested IO events
+     * Must be called int the loop thread
+     * */
+    void updateChannel(Channel* channel);
+
+    void assertInLoopThread()
+    {
+        ownLoop_->assertInLoopThread();
+    }
+private:
+    void fillActiveChannels(int numEvents, ChannelList* activeChannels);
+
+    using ChannelMap = std::map<int, Channel*>;
+    using PollfdList = std::vector<struct pollfd>;
+
+    Eventloop* ownLoop_;
+    PollfdList pollfds_;
+    ChannelMap channels_;
+};
+}
+
+#endif //S01_POLLER_H
